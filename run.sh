02916c2cdb44491f06ba77e9984a4d0e17b8a3af
Svrma <shrutivrma1301@gmail.com>
2026-03-05 15:37:20 +0530

    Fix command errors

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Deploying application using Ansible..."
-ansible-playbook ansible/deploy.yml -i ansible/inventory.ini
+ansible-playbook ansible/deploy.yml -i ansible/inventory.ini -K
 
 echo "Opening calculator..."
 docker exec -it scientific-calculator java -jar app.jar
